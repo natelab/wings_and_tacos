417ef2d20e297a4351959989b25df13c752ab897
Nathan <122789063+natelab@users.noreply.github.com>
2024-06-12 18:54:24 +0200

    Better

--- Source Code/menu_order.php
+++ Source Code/menu_order.php
@@ -76,24 +76,27 @@ $all_product = $conn->query($spl);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wings & Tacos</title>
 
-    <!--Icon for the Title of the website-->
+    <!-- Icon for the Title of the website -->
     <link rel="icon" href="Images/Logo - Copy.jpg" />
 
-    <!--Link to the CSS File-->
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="menu_order_style.css"/>
 
-    <!--CND Link to Font Awesome-->
+    <!-- CND Link to Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" 
     integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" 
     crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <!--Link to Fonts-->
+    <!-- Link to Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&family=Oswald:wght@200..700&family=Spicy+Rice&display=swap" rel="stylesheet">
 </head>
 <body>
-    <!--NAVIGATION BAR-->
+    <!-- NAVIGATION BAR -->
     <section class="navbar-section">
         <nav class="navbar">
             <a href="main.php"><img src="Images/Taskbar Logo.jpg" height="130"></a>
@@ -112,27 +115,25 @@ $all_product = $conn->query($spl);
     </section>
 
     <section class="menu">
-        <main class="main">
+    <div class="container">
+        <div class="row">
             <?php while($row = mysqli_fetch_assoc($all_product)) { ?>
-            <div class="card">
-                <div class="image">
-                    <a href="main.php"><img src="data:image/jpeg;base64,<?php echo base64_encode($row['img']); ?>" height="200"></a>
+            <div class="col-md-4 col-sm-6 mb-4">
+                <div class="card h-100">
+                    <div class="image">
+                        <a href="main.php"><img src="data:image/jpeg;base64,<?php echo base64_encode($row['img']); ?>" class="card-img-top" alt="<?php echo $row["prod_name"]; ?>"></a>
+                    </div>
+                    <div class="card-body text-center">
+                        <h5 class="caption"><?php echo $row["prod_name"]; ?></h5>
+                        <p class="price">R<?php echo $row["price"]; ?></p>
+                        <button onclick="window.location.href='login.php'" class="btn btn-primary add">Add to cart</button>
+                    </div>
                 </div>
-                <div class="caption">
-                    <p class="product_name"><?php echo $row["prod_name"]; ?></p>
-                    <p class="price"><b>R<?php echo $row["price"]; ?></b></p>
-                </div>
-                <form method="post" action="">
-                    <!--<input type="hidden" name="product_id" value="<?php echo $row['id']; ?>">-->
-                    <input type="hidden" name="product_name" value="<?php echo $row['prod_name']; ?>">
-                    <input type="hidden" name="product_price" value="<?php echo $row['price']; ?>">
-                    <input type="hidden" name="product_image" value="<?php echo base64_encode($row['img']); ?>">
-                    <button type="submit" name="add_to_cart" class="add">Add to cart</button>
-                </form>
             </div>
             <?php } ?>
-        </main>
-    </section>
+        </div>
+    </div>
+</section>
 
     <section class="cart">
         <h2>Shopping Cart</h2>
@@ -188,6 +189,37 @@ $all_product = $conn->query($spl);
         </form>
     </section>
 
-    
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
+
+<footer class="footer">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <img src="Images/Footer Logo.png" alt="Footer Logo" class="footer-logo">
+            </div>
+            <div class="col-md-6 text-right">
+                <div class="social-links">
+                    <p>Connect with us: <br><!-- Added text -->
+                        <a href="https://www.tiktok.com/@mamtimande333?lang=en"><i class="fab fa-tiktok fa-lg"></i></a>
+                        <a href="https://www.instagram.com/wings_tacos/"><i class="fab fa-instagram fa-lg"></i></a>
+                    </p>
+                </div>
+                <p class="copyright">
+                    Wings & Tacos 2024 ©. All rights reserved.
+                    <br><br> <!-- Added empty line space -->
+                    This Website Was Developed, Designed and Is Maintained by Nathan Tinashe Mazonde
+                    <br>
+                    <a href="https://www.linkedin.com/in/nathan-mazonde"><i class="fab fa-linkedin fa-lg"></i></a>
+                    <a href="https://github.com/natelab"><i class="fab fa-github fa-lg"></i></a>
+                </p>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </html>
